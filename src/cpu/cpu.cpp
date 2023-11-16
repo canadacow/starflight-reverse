6b490bfc3b931d52627c1c53a19b13809a7b6cb2
Dean Beeler <dean.beeler@oculus.com>
2023-11-16 10:14:46 -0800

    Make sure the cpu cpp file is included.

---
+++ src/cpu/cpu.cpp
@@ -0,0 +1,122 @@
+#include "cpu.h"
+#include <string.h>
+#include <assert.h>
+
+#include <stdio.h>
+#include "../emul/callstack.h"
+
+unsigned char m[SystemMemorySize];
+unsigned char *mem;
+unsigned short regsp;
+unsigned short regbp;
+unsigned short int regsi = 0x129; // current vocabulary address (the forth pc pointer)
+
+unsigned short regbx;
+
+unsigned long ComputeAddress(unsigned short segment, unsigned short offset)
+{
+    unsigned long addr = ((unsigned long)segment << 4) + offset;
+
+    // Nebula detection code (to see if the ship is actually in the nebula)
+    // reads directly from video memory
+
+    if(addr >= 0xA0000 && addr <= 0xAFFFF)
+    {
+        printf("Access to video memory from program itself.\n");
+        PrintCallstacktrace(regbx);
+        assert(0);
+    }
+    return addr;
+}
+
+void Write8(unsigned short offset, unsigned char x)
+{
+    Write8Long(StarflightBaseSegment, offset, x);
+}
+
+void Write8Long(unsigned short s, unsigned short o, unsigned char x)
+{
+    unsigned long addr = ComputeAddress(s, o);
+
+    m[addr] = x;
+
+    if(addr == 0x7d20)
+    {
+        printf("Hull at %d\n", m[addr]);
+        fflush(stdout);
+    }
+    
+    #if 0
+    constexpr uint16_t hullvalue = 0x63ef + 0x11;
+
+    if((s == StarflightBaseSegment) && ((o == hullvalue) || o == (hullvalue +1)))
+    {
+        uint16_t hull = Read16(0x63ef + 0x11);
+        //if(hull != 100)
+        {
+            printf("Hull at %d\n", hull);
+            fflush(stdout);
+        }
+    }
+    #endif
+}
+
+void Write16(unsigned short offset, unsigned short x)
+{
+    Write8Long(StarflightBaseSegment, offset, (x>>0)&0xFF);
+    Write8Long(StarflightBaseSegment, offset+1, (x>>8)&0xFF);
+}
+
+void Write16Long(unsigned short s, unsigned short o, unsigned short x)
+{
+    Write8Long(s, o, x&0xFF);
+    Write8Long(s, o +1, x>>8);
+}
+
+unsigned char Read8(unsigned short offset)
+{
+    return mem[offset];
+}
+
+unsigned char Read8Long(unsigned short s, unsigned short o)
+{
+    unsigned long addr = ComputeAddress(s, o);
+
+    return m[addr];
+}
+
+unsigned short Read16(unsigned short offset)
+{
+    unsigned short val = mem[offset+0] | (mem[offset+1]<<8);
+
+    return val;
+}
+
+unsigned short Read16Long(unsigned short s, unsigned short o)
+{
+    unsigned long addr = ComputeAddress(s, o);
+
+    return m[addr + 0] | (m[addr + 1]<<8);
+}
+
+void Push(unsigned short x)
+{
+    regsp -= 2;
+    Write16(regsp, x);
+}
+
+unsigned short Pop()
+{
+    unsigned short x = Read16(regsp);
+    regsp += 2;
+    return x;
+}
+
+void InitCPU()
+{
+    memset(m, 0, SystemMemorySize);
+    mem = &m[StarflightBaseSegment << 4];
+    Init8086(m);
+}
+
+
