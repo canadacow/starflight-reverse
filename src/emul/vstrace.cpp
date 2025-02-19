d4862461e7377be9e4f6f1e4e61a9ef2b561f0d8
Dean Beeler <dean.beeler@oculus.com>
2025-02-19 00:09:44 -0800

    Turn off user marks

--- src/emul/vstrace.cpp
+++ src/emul/vstrace.cpp
@@ -1,7 +1,6 @@
 #include "vstrace.h"
 
-#ifdef _WIN32
-#define DIAGHUB_ENABLE_TRACE_SYSTEM
+#if defined(_WIN32) && defined(DIAGHUB_ENABLE_TRACE_SYSTEM)
 #include "UserMarks.h"
 DIAGHUB_DECLARE_TRACE
 #endif
@@ -11,52 +10,61 @@ UserMarks& UserMarks::getInstance() {
     return instance;
 }
 
+
+// Active definitions for Windows with DIAGHUB_ENABLE_TRACE_SYSTEM
+#if defined(_WIN32) && defined(DIAGHUB_ENABLE_TRACE_SYSTEM)
+
 void UserMarks::initialize(const std::string& name) {
-#ifdef _WIN32
     USERMARKS_INITIALIZE(std::wstring(name.begin(), name.end()).c_str());
     USERMARKRANGE_INITIALIZE(std::wstring(name.begin(), name.end()).c_str());
-#else
-    // No-op for non-Windows platforms
-#endif
 }
 
 void UserMarks::emit(const std::string& message) {
-#ifdef _WIN32
     USERMARKS_EMIT(std::wstring(message.begin(), message.end()).c_str());
-#else
-    // No-op for non-Windows platforms
-#endif
 }
 
 void UserMarks::startTraceSystem() {
-#ifdef _WIN32
     DIAGHUB_START_TRACE_SYSTEM();
-#else
-    // No-op for non-Windows platforms
-#endif
 }
 
 void UserMarks::stopTraceSystem() {
-#ifdef _WIN32
     DIAGHUB_STOP_TRACE_SYSTEM();
-#else
-    // No-op for non-Windows platforms
-#endif
 }
 
 UserMarks::UserMarkRange::UserMarkRange(const std::string& name) {
-#ifdef _WIN32
     USERMARKRANGE_START(std::wstring(name.begin(), name.end()).c_str());
-#else
-    // No-op for non-Windows platforms
-#endif
 }
 
 UserMarks::UserMarkRange::~UserMarkRange() {
-#ifdef _WIN32
     USERMARKRANGE_END();
-#else
-    // No-op for non-Windows platforms
-#endif
 }
 
+#else
+
+// Inactive definitions for non-Windows platforms or without DIAGHUB_ENABLE_TRACE_SYSTEM
+
+void UserMarks::initialize(const std::string& name) {
+    // No-op for non-Windows platforms
+}
+
+void UserMarks::emit(const std::string& message) {
+    // No-op for non-Windows platforms
+}
+
+void UserMarks::startTraceSystem() {
+    // No-op for non-Windows platforms
+}
+
+void UserMarks::stopTraceSystem() {
+    // No-op for non-Windows platforms
+}
+
+UserMarks::UserMarkRange::UserMarkRange(const std::string& name) {
+    // No-op for non-Windows platforms
+}
+
+UserMarks::UserMarkRange::~UserMarkRange() {
+    // No-op for non-Windows platforms
+}
+
+#endif
